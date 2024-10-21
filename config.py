d22a81e08a729113b988e2d3035340fe3baabfcc
Schwarz Erik <schwarer@googlemail.com>
2024-10-21 15:56:34 +0200

    Update config.py
    First Create

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-config.py
+# config.py
+
+#Dies ist nur ein Entwicklungsstand!
+# Pfad zur DuckDB-Datenbankdatei
+DUCKDB_PATH = 'C:/data/tankerkoenig01/my_duckdb_instance.duckdb'
+
+# Weitere Optionen oder Einstellungen für die DuckDB-Instanz (optional)
+DUCKDB_SETTINGS = {
+    'enable_http': True,  # Beispiel für eine mögliche Einstellung
+    'cache_size': 1024    # Beispiel für den Cache in MB
